323c939006669ad0f39541efb5e9e1003a2a688d
Victor Gonzalez <dazedbutamused@gmail.com>
2019-09-25 10:26:51 -0700

    First Commit

---
+++ src/components/GameBoard/GameBoard.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+//we can just use Parentheses so that it creaets
+//an implicit return otherwise we would need
+//curly braces and return()
+const GameBoard = props => (
+    <div>
+        GameBoard
+    </div>
+);
+
+
+export default GameBoard;
+
